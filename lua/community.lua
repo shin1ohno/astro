15fc10a0b10dc46676a8bad8c1f0f8a3ad336392
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-03-31 00:58:08 +0900

    MCP hub

--- lua/community.lua
+++ lua/community.lua
@@ -13,4 +13,5 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.editing-support.mcphub-nvim" },
 }
