5136762f31b1ae16e1a6c2592ad5b0fd4dda5ce5
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-04-26 14:50:33 +0900

    feat: add obsidian.nvim and snacks.nvim plugins
    - Integrate obsidian.nvim for seamless Obsidian vault access
    
      - Configure iCloud Obsidian workspace for notes
    
      - Set up daily notes functionality with tags
    
      - Enable completion with nvim-cmp integration
    
    - Add snacks.nvim for enhanced UI and terminal experience
    
      - Configure lazygit integration with custom theme
    
      - Set up dashboard with neofetch, keymaps, recent files
    
      - Add git status section to dashboard
    
    Signed-off-by: Shin'ichi Ohno <shin1ohno@gmail.com>

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,45 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "epwalsh/pomo.nvim",
+  },
+  cmd = {
+    "ObsidianOpen",
+    "ObsidianNew",
+    "ObsidianQuickSwitch",
+    "ObsidianFollowLink",
+    "ObsidianBacklinks",
+    "ObsidianToday",
+    "ObsidianYesterday",
+    "ObsidianTemplate",
+    "ObsidianSearch",
+    "ObsidianLink",
+    "ObsidianLinkNew",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "mercari",
+        path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes/",
+      },
+    },
+    notes_dir = "notes",
+    log_level = vim.log.levels.INFO,
+    daily_notes = {
+      folder = "notes/dailies",
+      default_tags = { "daily-notes" },
+      template = nil,
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+  },
+}
