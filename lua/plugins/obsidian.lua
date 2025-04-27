1f2d9dd31c222a85f79c5cbdbfb53b9565a2f7a1
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-04-28 01:42:35 +0900

    fix(obsidian): trigger vault sync autocmd only on non-mac systems
    Simplify the BufWritePost handler so the rclone sync script runs only on Linux. This avoids executing an unnecessary (and currently unused) sync command on macOS while keeping the cross-platform behavior intact.
    
    Signed-off-by: Shin'ichi Ohno <shin1ohno@gmail.com>

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -33,25 +33,20 @@ return {
     "ObsidianLinkNew",
   },
   init = function()
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      pattern = "*.md",
-      callback = function()
-        local vault_sync_command
+    if not is_mac then
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*.md",
+        callback = function()
+          local vault_sync_command = "~/.setup_shin1ohno/obsidian_sync/sync.sh"
 
-        if is_mac then
-          vault_sync_command =
-            "/Users/sh1/.gvm/pkgsets/go1.21.3/global/bin/rclone bisync ~/Library/Mobile\\ Documents/iCloud~md~obsidian/Documents/ drive:obsidian"
-        else
-          vault_sync_command = "/usr/bin/rclone bisync ~/obsidian/ obsidian:obsidian"
-        end
-
-        vim.fn.jobstart(vault_sync_command, {
-          on_exit = function(_, code)
-            if code ~= 0 then print("Error: " .. code) end
-          end,
-        })
-      end,
-    })
+          vim.fn.jobstart(vault_sync_command, {
+            on_exit = function(_, code)
+              if code ~= 0 then print("Error: " .. code) end
+            end,
+          })
+        end,
+      })
+    end
   end,
   opts = {
     workspaces = {
