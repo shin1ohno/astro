e554bcd1551df4e161022975edf9de94d26c8b9b
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-08-20 19:36:54 +0900

    polish: enable soft wrap for markdown files
    Configure markdown files to use soft wrapping with proper line breaks
    and indentation for better readability when editing long-form content

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,3 +1,12 @@
 vim.cmd [[colorscheme nord]]
 vim.cmd [[autocmd VimEnter * if exists(':Dotenv') | exe 'Dotenv ~/.env' | endif]]
 vim.opt.conceallevel = 1 --for obsidian
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+  end,
+})
