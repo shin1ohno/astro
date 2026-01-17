1adf68662ca56d34cc4d06e696244005add1e739
Shin'ichi Ohno <shin1ohno@gmail.com>
2026-01-17 18:24:09 +0900

    polish: enable OSC 52 clipboard for remote sessions
    Allow copy/paste to work seamlessly over SSH, mosh, and tmux
    by using the OSC 52 escape sequence protocol.

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,6 +2,20 @@ vim.cmd [[colorscheme nord]]
 vim.cmd [[autocmd VimEnter * if exists(':Dotenv') | exe 'Dotenv ~/.env' | endif]]
 vim.opt.conceallevel = 1 --for obsidian
 
+-- OSC 52 clipboard support (works over SSH/mosh/tmux)
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
