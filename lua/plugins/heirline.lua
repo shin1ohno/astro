41e2eb8aa94fadb3625506e950f5cfb4d7e6cab4
Shin'ichi Ohno <shin1ohno@gmail.com>
2026-04-18 04:32:06 +0900

    plugins: wire roon-nvim (dev spec + heirline card)

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+  "rebelot/heirline.nvim",
+  optional = true,
+  opts = function(_, opts)
+    local ok, rh = pcall(require, "roon-nvim.heirline")
+    if not ok then
+      return opts
+    end
+    opts.statusline = opts.statusline or {}
+    -- Insert the Roon Now Playing component just before the final
+    -- NvChad nav/folder section so it sits on the right side but not
+    -- off-screen.
+    local pos = math.max(#opts.statusline, 1)
+    table.insert(opts.statusline, pos, rh.component())
+    return opts
+  end,
+}
---
+++ lua/plugins/roon.lua
@@ -0,0 +1,40 @@
+---@type LazySpec
+return {
+  {
+    dir = vim.fn.expand("~/ManagedProjects/roon-nvim"),
+    name = "roon-nvim",
+    dev = true,
+    lazy = false,
+    dependencies = {
+      "rebelot/heirline.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      zone = "Qutest",
+    },
+    config = function(_, opts)
+      require("roon-nvim").setup(opts)
+      require("telescope").load_extension("roon")
+    end,
+    keys = {
+      { "<leader>fR", "<cmd>Telescope roon search<cr>", desc = "Roon search" },
+      { "<leader>nR", "<cmd>Neotree roon focus<cr>", desc = "Roon browser" },
+      { "<leader>mp", "<cmd>RoonPlayPause<cr>", desc = "Roon play/pause" },
+      { "<leader>mn", "<cmd>RoonNext<cr>", desc = "Roon next" },
+      { "<leader>mb", "<cmd>RoonPrevious<cr>", desc = "Roon previous" },
+      { "<leader>mx", "<cmd>RoonStop<cr>", desc = "Roon stop" },
+      { "<leader>ms", "<cmd>RoonStatus<cr>", desc = "Roon status card" },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      if not vim.tbl_contains(opts.sources, "roon") then
+        table.insert(opts.sources, "roon")
+      end
+      return opts
+    end,
+  },
+}
